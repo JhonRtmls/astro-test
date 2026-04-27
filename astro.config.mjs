914acbafe8c84c1900112273986d64e46a20196b
JhonRtmls <jhonretamales96@gmail.com>
2026-04-27 09:07:28 -0300

    fixing try 1

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://JhoRtmls.github.io',
+  site: 'https://JhonRtmls.github.io',
   base: '/astro-test',
 });

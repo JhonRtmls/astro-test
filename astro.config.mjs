d114d2ceac86e7d03b85fd47e5c05015ffd81810
JhonRtmls <jhonretamales96@gmail.com>
2026-04-30 11:25:14 -0300

    chore: set site URL to GitHub Pages for staging tests and make all Schema/Canonical tags fully dynamic

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gestoo.cl',
+  site: 'https://jhonrtmls.github.io',
   base: '/astro-test',
   trailingSlash: 'never',
   security: {
